60ec6bdaba95d091c0feadacd81a7bff20bb1d5d
dong4j <dong4j@gmail.com>
2025-01-14 12:23:37 +0800

    修复配置获取的错误逻辑

--- scripts/helpers/xxx.js
+++ scripts/helpers/xxx.js
@@ -1,7 +1,8 @@
 // 根据主题动态返回不同的图片
 // /Users/dong4j/Developer/3.Knowledge/site/hexo/themes/anzhiyu/layout/includes/page/about.pug
+
 hexo.extend.helper.register("xxx", function (type) {
-  if (hexo.config.darkmode.enable) {
+  if (hexo.theme.config.darkmode.enable) {
     return "https://cdn.dong4j.site/source/image/20250104212459_bX6bH12g.webp";
   } else {
     return "https://cdn.dong4j.site/source/image/20250104212916_NmPboUM8.webp";
